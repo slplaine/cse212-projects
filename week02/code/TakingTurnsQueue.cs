2c68941057ed1192c5e55140c707ccbb77710e70
slplaine <simoneleca@gmail.com>
2026-03-21 07:33:24 -0300

    making activities week 2

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,10 +1,10 @@
 /// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
+/// This queue is circular. When people are added via AddPerson, then they are added to the
+/// back of the queue (per FIFO rules). When GetNextPerson is called, the next person
+/// in the queue is saved to be returned and then they are placed back into the back of the queue. Thus,
+/// each person stays in the queue and is given turns. When a person is added to the queue,
+/// a turns parameter is provided to identify how many turns they will be given. If the turns is 0 or
+/// less then they will stay in the queue forever. If a person is out of turns then they will
 /// not be added back into the queue.
 /// </summary>
 public class TakingTurnsQueue
@@ -26,9 +26,9 @@ public class TakingTurnsQueue
 
     /// <summary>
     /// Get the next person in the queue and return them. The person should
-    /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
+    /// go to the back of the queue again unless the turns variable shows that they
+    /// have no more turns left. Note that a turns value of 0 or less means the
+    /// person has an infinite number of turns. An error exception is thrown
     /// if the queue is empty.
     /// </summary>
     public Person GetNextPerson()
@@ -37,17 +37,20 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
-        {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
-            {
-                person.Turns -= 1;
-                _people.Enqueue(person);
-            }
 
-            return person;
+        Person person = _people.Dequeue();
+
+        if (person.Turns <= 0)
+        {
+            _people.Enqueue(person);
         }
+        else if (person.Turns > 1)
+        {
+            person.Turns -= 1;
+            _people.Enqueue(person);
+        }
+
+        return person;
     }
 
     public override string ToString()
