4ce8bd0c347c385fe5768c355d60717c803360a3
slplaine <simoneleca@gmail.com>
2026-03-28 04:03:34 -0300

    making activities

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -33,6 +33,12 @@ public class Maze
     public void MoveLeft()
     {
         // FILL IN CODE
+        var moves = _mazeMap[(_currX, _currY)];
+
+        if (!moves[0])
+            throw new InvalidOperationException("Can't go that way!");
+
+        _currX--;
     }
 
     /// <summary>
@@ -42,6 +48,12 @@ public class Maze
     public void MoveRight()
     {
         // FILL IN CODE
+        var moves = _mazeMap[(_currX, _currY)];
+
+        if (!moves[1])
+            throw new InvalidOperationException("Can't go that way!");
+
+         _currX++;
     }
 
     /// <summary>
@@ -51,6 +63,12 @@ public class Maze
     public void MoveUp()
     {
         // FILL IN CODE
+        var moves = _mazeMap[(_currX, _currY)];
+
+        if (!moves[2])
+            throw new InvalidOperationException("Can't go that way!");
+
+        _currY--;
     }
 
     /// <summary>
@@ -60,6 +78,12 @@ public class Maze
     public void MoveDown()
     {
         // FILL IN CODE
+        var moves = _mazeMap[(_currX, _currY)];
+
+        if (!moves[3])
+            throw new InvalidOperationException("Can't go that way!");
+
+         _currY++;
     }
 
     public string GetStatus()
